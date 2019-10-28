9667dd1f5293f1ac468cae3adca82974a58592ee
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2019-10-28 17:22:46 +0100

    Repackage contents

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ except ImportError:
 
 setup(
     name='DemoUnit',
-    version='0.0.1',
+    version='0.0.2',
     author='Shailesh Appukuttan, Andrew Davison',
     author_email='shailesh.appukuttan@unic.cnrs-gif.fr',
     packages=['DemoUnit',
+              'DemoUnit.tests',
               'DemoUnit.capabilities'],
     url='https://github.com/appukuttan-shailesh/DemoUnit',
     keywords = ['electrophysiology', 'electrical', 'testing', 'validation framework'],
